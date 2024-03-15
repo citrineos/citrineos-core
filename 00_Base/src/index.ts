e39e3d803cb6247dfc321cd3e50f81274d802486
thanaParis <thana.paris@s44.team>
2024-03-15 14:00:12 -0400

    adding OcppMessageRouter to export
    Signed-off-by: thanaParis <thana.paris@s44.team>

--- 00_Base/src/index.ts
+++ 00_Base/src/index.ts
@@ -8,7 +8,7 @@
 export { AbstractModuleApi, AsDataEndpoint, AsMessageEndpoint, HttpMethod, IModuleApi } from './interfaces/api';
 export { CacheNamespace, ICache } from './interfaces/cache/cache';
 export { AbstractCentralSystem, ClientConnection, ICentralSystem, IClientConnection } from './interfaces/centralsystem';
-export { AbstractMessageHandler, AbstractMessageSender, EventGroup, HandlerProperties, IMessage, IMessageConfirmation, IMessageContext, IMessageHandler, IMessageRouter, IMessageSender, Message, MessageOrigin, MessageState, RetryMessageError } from './interfaces/messages';
+export { AbstractMessageHandler, AbstractMessageSender, EventGroup, HandlerProperties, IMessage, IMessageConfirmation, IMessageContext, IMessageHandler, IMessageRouter, OcppMessageRouter, IMessageSender, Message, MessageOrigin, MessageState, RetryMessageError } from './interfaces/messages';
 export { AbstractModule, AsHandler, IModule } from './interfaces/modules';
 export { Call, CallAction, CallError, CallResult, ErrorCode, MessageTypeId, OcppError } from './ocpp/rpc/message';
 
