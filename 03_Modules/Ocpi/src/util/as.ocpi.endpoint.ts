130fcdbf292d9368d8a500be25e7286b450bc2b6
Elliot Sabitov <>
2024-04-29 16:15:58 -0400

    chore: cleanup adjustments for clarity

--- 03_Modules/Ocpi/src/util/as.ocpi.endpoint.ts
+++ 03_Modules/Ocpi/src/util/as.ocpi.endpoint.ts
@@ -3,12 +3,12 @@
 //
 // SPDX-License-Identifier: Apache 2.0
 
-import {AsDataEndpoint, HttpMethod, Namespace} from '@citrineos/base';
+import {AsDataEndpoint, HttpMethod} from '@citrineos/base';
 
 /**
  * Decorator for use in module API class to expose methods as REST data endpoints.
  *
- * @param {Namespace} namespace - The namespace value.
+ * @param {Url} url - The url value.
  * @param {HttpMethod} method - The HTTP method value.
  * @param {object} querySchema - The query schema value (optional).
  * @param {object} bodySchema - The body schema value (optional).
@@ -18,7 +18,7 @@ import {AsDataEndpoint, HttpMethod, Namespace} from '@citrineos/base';
  * @return {void} - No return value.
  */
 export const AsOcpiEndpoint = function (
-  namespace: string,
+  url: string,
   method: HttpMethod,
   querySchema?: object,
   bodySchema?: object,
@@ -26,15 +26,22 @@ export const AsOcpiEndpoint = function (
   headerSchema?: object,
   responseSchema?: object,
 ) {
+  // @ts-expect-error setting header
   headerSchema['authorization'] = { type: 'string', required: true }; // String authorization,
+  // @ts-expect-error setting header
   headerSchema['X-Request-ID'] = { type: 'string', required: true }; // String xRequestID,
+  // @ts-expect-error setting header
   headerSchema['X-Correlation-ID'] = { type: 'string', required: true }; // String xCorrelationID,
+  // @ts-expect-error setting header
   headerSchema['OCPI-from-country-code'] = { type: 'string', required: true }; // String ocPIFromCountryCode,
+  // @ts-expect-error setting header
   headerSchema['OCPI-from-party-id'] = { type: 'string', required: true }; // String ocPIFromPartyId,
+  // @ts-expect-error setting header
   headerSchema['OCPI-to-country-code'] = { type: 'string', required: true }; // String ocPIToCountryCode,
+  // @ts-expect-error setting header
   headerSchema['OCPI-to-party-id'] = { type: 'string', required: true }; // String ocPIToPartyId,
   return AsDataEndpoint(
-    namespace,
+    url,
     method,
     querySchema,
     bodySchema,
