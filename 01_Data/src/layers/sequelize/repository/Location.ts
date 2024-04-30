c921c9b77b4c3dc4c675cdc4d1ee16cb6c83d7ee
thanaParis <thana.paris@s44.team>
2024-04-30 16:41:53 -0400

    updating location repository to use inherited class methods to ensure events are emitted
    Signed-off-by: thanaParis <thana.paris@s44.team>

--- 01_Data/src/layers/sequelize/repository/Location.ts
+++ 01_Data/src/layers/sequelize/repository/Location.ts
@@ -2,11 +2,20 @@
 //
 // SPDX-License-Identifier: Apache 2.0
 
+import { CrudRepository, SystemConfig } from '@citrineos/base';
+import { Sequelize } from 'sequelize-typescript';
+import { Logger, ILogObj } from 'tslog';
 import { ChargingStation, type Location, SequelizeRepository } from '..';
 import { type ILocationRepository } from '../../..';
 
-export class LocationRepository extends SequelizeRepository<Location> implements ILocationRepository {
-  async readChargingStationByStationId(stationId: string): Promise<ChargingStation | null> {
-    return await ChargingStation.findByPk(stationId);
+export class SequelizeLocationRepository extends SequelizeRepository<Location> implements ILocationRepository {
+  chargingStation: CrudRepository<ChargingStation>;
+
+  constructor(config: SystemConfig, namespace: string, logger?: Logger<ILogObj>, sequelizeInstance?: Sequelize, chargingStation?: CrudRepository<ChargingStation>) {
+    super(config, namespace, logger, sequelizeInstance);
+    this.chargingStation = chargingStation ? chargingStation : new SequelizeRepository<ChargingStation>(config, namespace, logger, sequelizeInstance);
+  }
+  async readChargingStationByStationId(stationId: string): Promise<ChargingStation | undefined> {
+    return await this.chargingStation.readByKey(stationId);
   }
 }
