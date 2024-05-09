960a19887c8f8346908f4e013bfa9664506a5def
Elliot Sabitov <>
2024-05-09 15:55:08 -0400

    chore: cleanup

--- 03_Modules/Ocpi/src/util/class.validator.ts
+++ 03_Modules/Ocpi/src/util/class.validator.ts
@@ -4,19 +4,14 @@ import {ISchemaConverters} from 'class-validator-jsonschema/build/defaultConvert
 import {IOptions} from 'class-validator-jsonschema/build/options';
 import {Constructor} from './util';
 import type {SchemaObject} from 'openapi3-ts';
-import {ValidationMetadata} from "class-validator/types/metadata/ValidationMetadata";
-import {Price} from "../model/Price";
-// @ts-ignore
-import {defaultMetadataStorage} from 'class-transformer/cjs/storage.js'
-// import * as defaultMetadataStorage from "class-transformer/cjs/storage";
+import {ValidationMetadata} from 'class-validator/types/metadata/ValidationMetadata';
+// @ts-expect-error importing js directly from class-transformer
+import {defaultMetadataStorage} from 'class-transformer/cjs/storage.js';
 
 const refPointerPrefix = '#/components/schemas/';
 
 
 function getPropType(target: object, property: string) {
-  if (Reflect.getMetadata('design:type', target, "total_energy_cost")) {
-    console.log(Reflect.getMetadata('design:type', target, "total_energy_cost").is(Price));
-  }
   return Reflect.getMetadata('design:type', target, property);
 }
 
@@ -27,70 +22,11 @@ type Options = IOptions & {
 };
 
 function nestedClassToJsonSchema(clz: Constructor<any>, options: Partial<Options>): SchemaObject {
-  return targetConstructorToSchema(clz, options) as any
+  return targetConstructorToSchema(clz, options) as any;
 }
 
 
 const additionalConverters: ISchemaConverters = {
-  /* [IS_ARRAY]: (meta: ValidationMetadata, options: Options) => {
-    if (meta.propertyName === 'tariffs' || meta.propertyName === 'signed_data') {
-      console.log('IS_ARRAY', meta, options);
-    }
-    return {
-      items: {},
-      type: 'array',
-      confused: true
-    };
-  }, */
-  /* [ValidationTypes.CONDITIONAL_VALIDATION]: (meta: ValidationMetadata, options: Options) => {
-    const schema1 = constraintToSchema(meta.constraints[0]);
-
-    const typeMeta = options.classTransformerMetadataStorage
-      ? options.classTransformerMetadataStorage.findTypeMetadata(meta.target as any, meta.propertyName)
-      : null;
-
-    const childType = typeMeta
-      ? typeMeta.typeFunction()
-      : null;
-
-    const schema = targetToSchema(childType, options);
-
-    // console.log('CONDITIONAL_VALIDATION', meta, options, schema1, typeMeta, childType, schema);
-
-    // if (!options.definitions) {
-    //   options.definitions = {};
-    // }
-    //
-    // if (schema.$ref && !options.definitions[childType.name]) {
-    //   options.definitions[childType.name] = nestedClassToJsonSchema(childType, options);
-    // }
-    //
-    // const name = meta.target.name;
-    //
-    // if (schema.$ref === '#/components/schemas/Object') {
-    //   schema.$ref = `${refPointerPrefix}${name}`;
-    // }
-
-    if (meta.propertyName === 'tariffs' || meta.propertyName === 'signed_data') {
-      console.log('schema', schema);
-      if (!!schema && !!schema.$ref && schema.$ref.indexOf(options.refPointerPrefix) > -1) {
-        return {
-          anyOf: [
-            {$ref: schema.$ref},
-            {
-              type: null
-            }
-          ]
-        };
-      }
-    }
-
-    return schema;
-
-    // const schema = targetToSchema(childType, options);
-    //
-    // return schema;
-  }, */
   [ValidationTypes.NESTED_VALIDATION]: (meta: ValidationMetadata, options: Options) => {
     if (typeof meta.target === 'function') {
 
@@ -136,23 +72,6 @@ const additionalConverters: ISchemaConverters = {
       } else {
         return schema;
       }
-      /*
-
-            if (meta.propertyName === 'tariffs' || meta.propertyName === 'signed_data') {
-              console.log('schema');
-              const metadataStorage = getMetadataStorage();
-              const validations = metadataStorage.getTargetValidationMetadatas(meta.target, meta.target.name, false, false);
-              const propertyValidations = validations.filter(v => v.propertyName === "signed_data");
-              // const metadata = Reflect.getMetadataKeys(meta.target, meta.propertyName);
-              const some = Reflect.getMetadata('isOptional', meta.target.prototype, meta.propertyName);
-              console.log(propertyValidations, some);
-              schema.$ref += 'heerere';
-            }
-
-            console.log(childType instanceof CdrToken);
-
-
-      return schema;  */
     }
   },
 } as any;
@@ -179,14 +98,6 @@ function targetToSchema(type: any, options: IOptions): any | void {
       return {type: 'boolean'};
     }
 
-    return {$ref: options.refPointerPrefix + type.name}
-  }
-}
-
-function constraintToSchema(primitive: any): SchemaObject | void {
-  const primitives = ['string', 'number', 'boolean'];
-  const type = typeof primitive;
-  if (primitives.includes(type)) {
-    return {type} as SchemaObject;
+    return {$ref: options.refPointerPrefix + type.name};
   }
 }
--- 03_Modules/Ocpi/src/util/optional.ts
+++ 03_Modules/Ocpi/src/util/optional.ts
@@ -1,6 +1,11 @@
 import {IsOptional} from 'class-validator';
 import {Cdr} from "../model/Cdr";
 
+/**
+ * Optional - mimics @IsOptional decorator but adds custom metadata so that it is easily available in
+ * Reflect to check if the property is optional
+ * @constructor
+ */
 export const Optional = () => {
   return function (object: NonNullable<unknown>, propertyName: string) {
     // Apply the standard @IsOptional() decorator
