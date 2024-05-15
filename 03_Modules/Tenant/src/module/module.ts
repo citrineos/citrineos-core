4edee91f1a6db1c967befdf3dd39772086cb1ead
Venus Yu <venus.yu@s44.team>
2024-05-15 11:01:29 -0400

    tenant module: adding tenant module-related base files into 03_Modules/Tenant

---
+++ 03_Modules/Tenant/src/module/module.ts
@@ -0,0 +1,10 @@
+// Copyright (c) 2023 S44, LLC
+// Copyright Contributors to the CitrineOS Project
+//
+// SPDX-License-Identifier: Apache 2.0
+
+import { AbstractModule } from "@citrineos/base";
+
+export class TenantModule extends AbstractModule {
+  
+}
