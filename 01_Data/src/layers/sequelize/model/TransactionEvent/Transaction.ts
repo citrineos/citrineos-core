868583cf37d6ff8a006ac36dd5dae7c84f861b2a
Elliot Sabitov <>
2024-07-02 15:38:02 -0400

    feat: created helper builder

--- 01_Data/src/layers/sequelize/model/TransactionEvent/Transaction.ts
+++ 01_Data/src/layers/sequelize/model/TransactionEvent/Transaction.ts
@@ -62,4 +62,34 @@ export class Transaction extends Model implements TransactionType {
   declare remoteStartId?: number;
 
   declare customData?: CustomDataType;
+
+  static buildTransaction(
+    id: string, // todo temp
+    stationId: string,
+    transactionId: string,
+    isActive: boolean,
+    transactionEvents: TransactionEventRequest[],
+    meterValues: MeterValueType[],
+    chargingState?: ChargingStateEnumType,
+    timeSpentCharging?: number,
+    totalKwh?: number,
+    stoppedReason?: ReasonEnumType,
+    remoteStartId?: number,
+    customData?: CustomDataType,
+  ) {
+    const transaction = new Transaction();
+    transaction.id = id;
+    transaction.stationId = stationId;
+    transaction.transactionId = transactionId;
+    transaction.isActive = isActive;
+    transaction.transactionEvents = transactionEvents;
+    transaction.meterValues = meterValues;
+    transaction.chargingState = chargingState;
+    transaction.timeSpentCharging = timeSpentCharging;
+    transaction.totalKwh = totalKwh;
+    transaction.stoppedReason = stoppedReason;
+    transaction.remoteStartId = remoteStartId;
+    transaction.customData = customData;
+    return transaction;
+  }
 }
