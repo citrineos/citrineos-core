944c2bc5a13667a4f07d9d1b84b28cb7d6c0764b
Venus Yu <venus.yu@s44.team>
2024-07-17 11:48:56 -0400

    locations admin endpoint: updating Location repository with create + update methods for locations to make OCPI integration smoother + updating coordinates type to Point for ease of use

--- 01_Data/src/layers/sequelize/repository/Location.ts
+++ 01_Data/src/layers/sequelize/repository/Location.ts
@@ -32,7 +32,7 @@ export class SequelizeLocationRepository extends SequelizeRepository<Location> i
   }
 
   async addStatusNotificationToChargingStation(stationId: string, statusNotification: StatusNotificationRequest): Promise<void> {
-    this.statusNotification.create(
+    await this.statusNotification.create(
       StatusNotification.build({
         stationId,
         ...statusNotification,
@@ -50,5 +50,78 @@ export class SequelizeLocationRepository extends SequelizeRepository<Location> i
     };
 
     return this.chargingStation.readAllByQuery(query);
+  }
+
+  async createOrUpdateLocationWithChargingStations(location: Partial<Location>): Promise<Location> {
+    let savedLocation;
+    if (location.id) {
+      const result = await this.readOrCreateByQuery({
+        where: {
+          id: location.id
+        },
+        defaults: {
+          name: location.name,
+          address: location.address,
+          city: location.city,
+          postalCode: location.postalCode,
+          state: location.state,
+          country: location.country,
+          coordinates: location.coordinates,
+        },
+      });
+
+      savedLocation = result[0];
+      const locationCreated = result[1];
+
+      if (!locationCreated) {
+        const values: Partial<Location> = {};
+        values.name = location.name ?? undefined;
+        values.address = location.address ?? undefined;
+        values.city = location.city ?? undefined;
+        values.postalCode = location.postalCode ?? undefined;
+        values.state = location.state ?? undefined;
+        values.country = location.country ?? undefined;
+        values.coordinates = location.coordinates ?? undefined;
+
+        await this.updateByKey({ ...values}, savedLocation.id);
+      }
+    } else {
+      savedLocation = await this.create(Location.build({ ...location }));
+    }
+
+    if (location.chargingPool && location.chargingPool.length > 0) {
+      for (let chargingStation of location.chargingPool) {
+        chargingStation.locationId = savedLocation.id;
+        await this.createOrUpdateChargingStation(chargingStation);
+      }
+    }
+
+    return savedLocation.reload({ include: ChargingStation });
+  }
+
+  async createOrUpdateChargingStation(chargingStation: ChargingStation): Promise<ChargingStation> {
+    if (chargingStation.id) {
+      const [savedChargingStation, chargingStationCreated] = await this.chargingStation.readOrCreateByQuery({
+        where: {
+          id: chargingStation.id
+        },
+        defaults: {
+          id: chargingStation.id,
+          locationId: chargingStation.locationId
+        },
+      });
+      if (!chargingStationCreated) {
+        await this.chargingStation.updateByKey(
+          {
+            locationId: chargingStation.locationId
+          },
+          savedChargingStation.id,
+        );
+      }
+
+      return savedChargingStation;
+    } else {
+      return await this.chargingStation.create(ChargingStation.build({ ...chargingStation }));
+    }
   }
 }
