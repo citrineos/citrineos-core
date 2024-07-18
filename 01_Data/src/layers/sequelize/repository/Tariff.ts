7ecb42a3ec540542ab54c03a631d220f39419591
Daniel Yoo <daniel.yoo@s44.team>
2024-07-18 16:11:16 -0400

    feat: adding new method to fetch by multiple stationIds for tariffs

--- 01_Data/src/layers/sequelize/repository/Tariff.ts
+++ 01_Data/src/layers/sequelize/repository/Tariff.ts
@@ -8,10 +8,21 @@ import { Tariff } from '../model/Tariff';
 import { Sequelize } from 'sequelize-typescript';
 import { SystemConfig } from '@citrineos/base';
 import { ILogObj, Logger } from 'tslog';
+import { Op } from 'sequelize';
 
 export class SequelizeTariffRepository extends SequelizeRepository<Tariff> implements ITariffRepository {
   constructor(config: SystemConfig, logger?: Logger<ILogObj>, sequelizeInstance?: Sequelize) {
     super(config, Tariff.MODEL_NAME, logger, sequelizeInstance);
+  }
+
+  async findByStationIds(stationIds: string[]): Promise<Tariff[] | undefined> {
+    return super.readAllByQuery({
+      where: {
+        stationId: {
+          [Op.in]: stationIds
+        },
+      },
+    });
   }
 
   async findByStationId(stationId: string): Promise<Tariff | undefined> {
@@ -33,7 +44,7 @@ export class SequelizeTariffRepository extends SequelizeRepository<Tariff> imple
         this.emit('updated', [updatedTariff]);
         return updatedTariff;
       }
-      const createdTariff = await tariff.save({ transaction });
+      const createdTariff = await tariff.save({transaction});
       this.emit('created', [createdTariff]);
       return createdTariff;
     });
@@ -42,7 +53,7 @@ export class SequelizeTariffRepository extends SequelizeRepository<Tariff> imple
   async readAllByQuerystring(query: TariffQueryString): Promise<Tariff[]> {
     return super.readAllByQuery({
       where: {
-        ...(query.id && { id: query.id }),
+        ...(query.id && {id: query.id}),
       },
     });
   }
@@ -53,7 +64,7 @@ export class SequelizeTariffRepository extends SequelizeRepository<Tariff> imple
     }
     return super.deleteAllByQuery({
       where: {
-        ...(query.id && { id: query.id }),
+        ...(query.id && {id: query.id}),
       },
     });
   }
