5e04e1aad146b090497229987b9c9025c2470d15
Christian Weissmann <christian.weissmann@s44.team>
2024-07-26 18:05:22 +0200

    refactor: make jest config typescript for consistency
    Signed-off-by: Christian Weissmann <christian.weissmann@s44.team>

--- 03_Modules/Configuration/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+.tsx?$': ['ts-jest', {}],
-  },
-  displayName: 'Configuration Module',
-};
