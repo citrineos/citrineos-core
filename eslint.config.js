9c03f022d64ee03654b9e8805537f34971b1fe4f
Elliot Sabitov <>
2024-08-15 13:14:58 -0400

    chore: updating eslint / tslint setup to newer version and removing no longer supported files but instead using the new eslint.config.js following all recommended settings overriding only the no-explicit-any and no-unused-vars values and setting the ignores

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    ignores: [
+      '**/DirectusExtensions/**',
+      '**/Swarm/**',
+      '**/dist/**',
+      '**/lib/**',
+    ],
+  },
+);
