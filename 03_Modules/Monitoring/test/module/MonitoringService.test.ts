d245d6b4c7497d31dbf75957e18573b07368a8f1
Venus Yu <venus.yu@s44.team>
2024-08-15 09:04:12 -0400

    clear variable monitoring tests: adding jest config to package json test script + adding unit tests for processClearMonitoringResult

--- 03_Modules/Monitoring/test/module/MonitoringService.test.ts
+++ 03_Modules/Monitoring/test/module/MonitoringService.test.ts
@@ -1,5 +1,10 @@
 import { IVariableMonitoringRepository } from '@citrineos/data/dist/interfaces/repositories';
 import { MonitoringService } from '../../src/module/MonitoringService';
+import {
+  ClearMonitoringResultType,
+  ClearMonitoringStatusEnumType,
+} from '@citrineos/base';
+import { aClearMonitoringResult } from '../providers/Monitoring';
 
 describe('MonitoringService', () => {
   let mockVariableMonitoringRepository: jest.Mocked<IVariableMonitoringRepository>;
@@ -11,5 +16,48 @@ describe('MonitoringService', () => {
     } as unknown as jest.Mocked<IVariableMonitoringRepository>;
 
     monitoringService = new MonitoringService(mockVariableMonitoringRepository);
+  });
+
+  describe('processClearMonitoringResult', () => {
+    it('should reject variable monitoring because clear monitoring result status is either Accepted or NotFound', async () => {
+      const monitoringResults: [
+        ClearMonitoringResultType,
+        ...ClearMonitoringResultType[],
+      ] = [
+        aClearMonitoringResult(),
+        aClearMonitoringResult(
+          (cmr) => (cmr.status = ClearMonitoringStatusEnumType.NotFound),
+        ),
+      ];
+
+      await monitoringService.processClearMonitoringResult(
+        'stationId',
+        monitoringResults,
+      );
+
+      expect(
+        mockVariableMonitoringRepository.rejectVariableMonitoringByIdAndStationId,
+      ).toHaveBeenCalledTimes(monitoringResults.length);
+    });
+
+    it('should not reject variable monitoring because  clear monitoring result status is Rejected (so neither Accepted nor NotFound)', async () => {
+      const monitoringResults: [
+        ClearMonitoringResultType,
+        ...ClearMonitoringResultType[],
+      ] = [
+        aClearMonitoringResult(
+          (cmr) => (cmr.status = ClearMonitoringStatusEnumType.Rejected),
+        ),
+      ];
+
+      await monitoringService.processClearMonitoringResult(
+        'stationId',
+        monitoringResults,
+      );
+
+      expect(
+        mockVariableMonitoringRepository.rejectVariableMonitoringByIdAndStationId,
+      ).not.toHaveBeenCalled();
+    });
   });
 });
