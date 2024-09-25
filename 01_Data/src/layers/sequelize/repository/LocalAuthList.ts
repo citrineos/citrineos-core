75208f189f96ffffef9fedc68c33367e3a8861a0
Diego Sanabria <diegosanabria@MacBook-Pro-de-Diego.local>
2024-09-25 11:44:41 -0500

    Bumped typescript eslint to V8.7.0, added new rule for no empty object type, fixed small issues

--- 01_Data/src/layers/sequelize/repository/LocalAuthList.ts
+++ 01_Data/src/layers/sequelize/repository/LocalAuthList.ts
@@ -2,6 +2,7 @@
 // Copyright Contributors to the CitrineOS Project
 //
 // SPDX-License-Identifier: Apache 2.0
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { AuthorizationData, CrudRepository, deepDirectionalEqual, SystemConfig, UpdateEnumType } from '@citrineos/base';
 import { Sequelize } from 'sequelize-typescript';
