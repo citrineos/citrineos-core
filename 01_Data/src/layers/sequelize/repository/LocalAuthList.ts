7d9ae2a74c5815e10c3f4b3f3b1262d73565b302
Christian Weissmann <christian.weissmann@s44.team>
2024-10-04 13:11:07 +0200

    fix: remove `_` prefix so deconstruction work correctly
    Signed-off-by: Christian Weissmann <christian.weissmann@s44.team>

--- 01_Data/src/layers/sequelize/repository/LocalAuthList.ts
+++ 01_Data/src/layers/sequelize/repository/LocalAuthList.ts
@@ -72,7 +72,7 @@ export class SequelizeLocalAuthListRepository extends SequelizeRepository<LocalL
         groupIdTokenId: auth.idTokenInfo?.groupIdTokenId,
       });
 
-      const { id, _idTokenInfoId, _idTokenInfo, ...authorizationFields } = auth;
+      const { id, idTokenInfoId, idTokenInfo, ...authorizationFields } = auth;
       const localListAuthorization = await this.localListAuthorization.create(
         LocalListAuthorization.build({
           ...authorizationFields,
