e2cf3dc00c9138491adb76d112ace263681bf940
Zihe Cheng <lydiazcheng@users.noreply.github.com>
2025-01-15 18:20:41 -0500

    Issue 92: fix errors
    Signed-off-by: Zihe Cheng <lydiazcheng@users.noreply.github.com>

--- 01_Data/src/layers/sequelize/mapper/1.6/BootMapper.ts
+++ 01_Data/src/layers/sequelize/mapper/1.6/BootMapper.ts
@@ -8,29 +8,31 @@ import { AbstractMapper } from '../AbstractMapper';
 import { Boot } from '../../model/Boot';
 
 export class BootMapper extends AbstractMapper {
-  lastBootTime?: string;
-  heartbeatInterval?: number;
-  bootRetryInterval?: number;
+  id: string;
+  lastBootTime?: string | null;
+  heartbeatInterval?: number | null;
+  bootRetryInterval?: number | null;
   status: OCPP1_6.BootNotificationResponseStatus;
-  changeConfigurationsOnPending?: boolean;
-  getConfigurationsOnPending?: boolean;
+  changeConfigurationsOnPending?: boolean | null;
+  getConfigurationsOnPending?: boolean | null;
 
   constructor(boot: Boot) {
     super();
-    try {
-      this.status = boot.status as OCPP1_6.BootNotificationResponseStatus;
-      this.lastBootTime = boot.lastBootTime;
-      this.heartbeatInterval = boot.heartbeatInterval;
-      this.bootRetryInterval = boot.bootRetryInterval;
-      this.changeConfigurationsOnPending = boot.changeConfigurationsOnPending;
-      this.getConfigurationsOnPending = boot.getConfigurationsOnPending;
-    } catch (error) {
-      throw new Error(`Generate BootMapper from Boot failed: ${error}`);
+    if (!Object.values(OCPP1_6.BootNotificationResponseStatus).includes(boot.status as OCPP1_6.BootNotificationResponseStatus)) {
+      throw new Error(`Invalid boot status: ${boot.status}`);
     }
+    this.id = boot.id;
+    this.status = boot.status as OCPP1_6.BootNotificationResponseStatus;
+    this.lastBootTime = boot.lastBootTime;
+    this.heartbeatInterval = boot.heartbeatInterval;
+    this.bootRetryInterval = boot.bootRetryInterval;
+    this.changeConfigurationsOnPending = boot.changeConfigurationsOnPending;
+    this.getConfigurationsOnPending = boot.getConfigurationsOnPending;
   }
 
   toModel(): Boot {
     return {
+      id: this.id,
       lastBootTime: this.lastBootTime,
       heartbeatInterval: this.heartbeatInterval,
       bootRetryInterval: this.bootRetryInterval,
