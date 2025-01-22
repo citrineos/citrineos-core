91429f6d2d3d1efd00b7dffb59d60bb5ffe39ee9
Zihe Cheng <lydiazcheng@users.noreply.github.com>
2025-01-22 15:03:46 -0500

    Issue 70: restructure mapper
    Signed-off-by: Zihe Cheng <lydiazcheng@users.noreply.github.com>

--- 01_Data/src/layers/sequelize/mapper/AbstractMapper.ts
+++ 01_Data/src/layers/sequelize/mapper/AbstractMapper.ts
@@ -2,8 +2,15 @@
 //
 // SPDX-License-Identifier: Apache 2.0
 
-import { Model } from 'sequelize-typescript';
+import { validateSync } from 'class-validator';
 
-export abstract class AbstractMapper {
-  abstract toModel(): Model;
+export abstract class AbstractMapper<T> {
+  abstract toModel(): T;
+
+  protected validate(): void {
+    const errors = validateSync(this, { validationError: { target: false } });
+    if (errors.length > 0) {
+      throw new Error('Validation failed: ' + JSON.stringify(errors));
+    }
+  }
 }
--- 01_Data/test/layers/sequelize/mapper/2.0.1/BootMapper.test.ts
+++ 01_Data/test/layers/sequelize/mapper/2.0.1/BootMapper.test.ts
@@ -1,76 +1,45 @@
 import { expect } from '@jest/globals';
-import { Boot, VariableAttribute } from '../../../../../src';
-import { faker } from '@faker-js/faker';
-import { OCPP2_0_1 } from '@citrineos/base';
+import { Boot } from '../../../../../src';
 import { BootMapper } from '../../../../../src/layers/sequelize/mapper/2.0.1';
+import { aBoot } from '../../../../providers/Boot';
 
 describe('BootMapper', () => {
   describe('map Boot and BootMapper', () => {
-    const givenSetVariable: OCPP2_0_1.SetVariableResultType = {
-      attributeStatus: OCPP2_0_1.SetVariableStatusEnumType.Accepted,
-      component: {
-        name: faker.lorem.word(),
-        instance: faker.string.uuid(),
-      },
-      variable: {
-        name: faker.lorem.word(),
-        instance: faker.string.uuid(),
-      },
-      customData: {
-        vendorId: faker.string.uuid(),
-      },
-      attributeType: OCPP2_0_1.AttributeEnumType.Actual,
-      attributeStatusInfo: {
-        reasonCode: faker.lorem.word(),
-        additionalInfo: faker.lorem.sentence(),
-      },
-    };
-    const givenBoot: Boot = {
-      id: faker.string.uuid(),
-      lastBootTime: faker.date.recent().toISOString(),
-      heartbeatInterval: faker.number.int({ min: 30, max: 3600 }),
-      bootRetryInterval: faker.number.int({ min: 30, max: 3600 }),
-      status: 'Pending',
-      getBaseReportOnPending: false,
-      variablesRejectedOnLastBoot: [givenSetVariable],
-      pendingBootSetVariables: [
-        {
-          stationId: faker.string.uuid(),
-          evseDatabaseId: faker.number.int({ min: 0, max: 100 }),
-          dataType: OCPP2_0_1.DataEnumType.string,
-          type: OCPP2_0_1.AttributeEnumType.Actual,
-          value: 'test',
-          mutability: OCPP2_0_1.MutabilityEnumType.ReadWrite,
-          persistent: false,
-          constant: false,
-          generatedAt: faker.date.recent().toISOString(),
-          variableId: faker.number.int({ min: 0, max: 100 }),
-          componentId: faker.number.int({ min: 0, max: 100 }),
-        } as VariableAttribute,
-      ],
-      bootWithRejectedVariables: faker.datatype.boolean(),
-    } as Boot;
-
     it('should be equal after mapping', () => {
-      const actualMapper = new BootMapper(givenBoot);
+      const givenBoot = aBoot();
 
+      const actualMapper = BootMapper.fromModel(givenBoot);
       expect(actualMapper).toBeTruthy();
-      expect(actualMapper.toModel()).toEqual(givenBoot);
+      expect(actualMapper.id).toBe(givenBoot.id);
+      expect(actualMapper.lastBootTime).toBe(givenBoot.lastBootTime);
+      expect(actualMapper.heartbeatInterval).toBe(givenBoot.heartbeatInterval);
+      expect(actualMapper.bootRetryInterval).toBe(givenBoot.bootRetryInterval);
+      expect(actualMapper.status).toBe(givenBoot.status);
+      expect(actualMapper.statusInfo).toEqual(givenBoot.statusInfo);
+      expect(actualMapper.getBaseReportOnPending).toBe(givenBoot.getBaseReportOnPending);
+      expect(actualMapper.pendingBootSetVariables).toEqual(givenBoot.pendingBootSetVariables);
+      expect(actualMapper.variablesRejectedOnLastBoot).toEqual(givenBoot.variablesRejectedOnLastBoot);
+      expect(actualMapper.bootWithRejectedVariables).toEqual(givenBoot.bootWithRejectedVariables);
+      expect(actualMapper.customData).toEqual(givenBoot.customData);
     });
 
     it('should throw error when missing required fields', () => {
-      const bootMissingRequiredFields: Boot = { ...givenBoot } as Boot;
+      const bootMissingRequiredFields: Boot = aBoot();
       bootMissingRequiredFields.variablesRejectedOnLastBoot = null;
 
-      expect(() => new BootMapper(bootMissingRequiredFields)).toThrowError('Missing variablesRejectedOnLastBoot');
+      expect(() => BootMapper.fromModel(bootMissingRequiredFields)).toThrowError(
+        `Validation failed: [{"value":null,"property":"variablesRejectedOnLastBoot","children":[],"constraints":{"isNotEmpty":"variablesRejectedOnLastBoot should not be empty"}}]`,
+      );
     });
 
     it('should throw error when invalid boot status', () => {
-      const bootWithInvalidStatus: Boot = { ...givenBoot } as Boot;
+      const bootWithInvalidStatus: Boot = aBoot();
       console.log(`bootWithInvalidStatus: ${JSON.stringify(bootWithInvalidStatus)}`, `status: ${bootWithInvalidStatus}`);
       bootWithInvalidStatus.status = 'InvalidStatus';
 
-      expect(() => new BootMapper(bootWithInvalidStatus)).toThrowError('Invalid boot status: InvalidStatus');
+      expect(() => BootMapper.fromModel(bootWithInvalidStatus)).toThrowError(
+        `Validation failed: [{"value":"InvalidStatus","property":"status","children":[],"constraints":{"isEnum":"status must be one of the following values: Accepted, Pending, Rejected"}}]`,
+      );
     });
   });
 });
