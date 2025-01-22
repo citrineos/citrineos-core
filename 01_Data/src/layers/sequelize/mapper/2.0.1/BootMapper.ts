f437784a4dae7c4647b5de25a51b555a7edab2a2
Zihe Cheng <lydiazcheng@users.noreply.github.com>
2025-01-22 15:03:46 -0500

    Issue 70: restructure mapper
    Signed-off-by: Zihe Cheng <lydiazcheng@users.noreply.github.com>

--- 01_Data/src/layers/sequelize/mapper/2.0.1/BootMapper.ts
+++ 01_Data/src/layers/sequelize/mapper/2.0.1/BootMapper.ts
@@ -5,62 +5,81 @@
 import { OCPP2_0_1 } from '@citrineos/base';
 import { AbstractMapper } from '../AbstractMapper';
 import { Boot } from '../../model/Boot';
+import { IsEnum, IsNotEmpty } from 'class-validator';
 
-export class BootMapper extends AbstractMapper {
+export class BootMapper extends AbstractMapper<Boot> {
   id: string;
   lastBootTime?: string | null;
   heartbeatInterval?: number | null;
   bootRetryInterval?: number | null;
+  @IsEnum(OCPP2_0_1.RegistrationStatusEnumType)
   status: OCPP2_0_1.RegistrationStatusEnumType;
   statusInfo?: OCPP2_0_1.StatusInfoType | null;
   getBaseReportOnPending?: boolean | null;
   pendingBootSetVariables?: OCPP2_0_1.VariableAttributeType[] | null;
+  @IsNotEmpty()
   variablesRejectedOnLastBoot: OCPP2_0_1.SetVariableResultType[];
   bootWithRejectedVariables?: boolean | null;
   customData?: OCPP2_0_1.CustomDataType | null;
 
-  constructor(boot: Boot) {
+  constructor(
+    id: string,
+    status: OCPP2_0_1.RegistrationStatusEnumType,
+    variablesRejectedOnLastBoot: OCPP2_0_1.SetVariableResultType[],
+    lastBootTime?: string | null,
+    heartbeatInterval?: number | null,
+    bootRetryInterval?: number | null,
+    statusInfo?: OCPP2_0_1.StatusInfoType | null,
+    getBaseReportOnPending?: boolean | null,
+    pendingBootSetVariables?: OCPP2_0_1.VariableAttributeType[] | null,
+    bootWithRejectedVariables?: boolean | null,
+    customData?: OCPP2_0_1.CustomDataType | null,
+  ) {
     super();
-    if (!Object.values(OCPP2_0_1.RegistrationStatusEnumType).includes(boot.status as OCPP2_0_1.RegistrationStatusEnumType)) {
-      throw new Error(`Invalid boot status: ${boot.status}`);
-    }
-    if (!boot.variablesRejectedOnLastBoot) {
-      throw new Error('Missing variablesRejectedOnLastBoot');
-    }
-    this.id = boot.id;
-    this.status = boot.status as OCPP2_0_1.RegistrationStatusEnumType;
-    this.variablesRejectedOnLastBoot = boot.variablesRejectedOnLastBoot as OCPP2_0_1.SetVariableResultType[];
-    this.fromModel(boot);
+    this.id = id;
+    this.status = status;
+    this.variablesRejectedOnLastBoot = variablesRejectedOnLastBoot;
+    this.lastBootTime = lastBootTime;
+    this.heartbeatInterval = heartbeatInterval;
+    this.bootRetryInterval = bootRetryInterval;
+    this.statusInfo = statusInfo;
+    this.getBaseReportOnPending = getBaseReportOnPending;
+    this.pendingBootSetVariables = pendingBootSetVariables;
+    this.bootWithRejectedVariables = bootWithRejectedVariables;
+    this.customData = customData;
+
+    this.validate();
   }
 
   toModel(): Boot {
-    try {
-      return {
-        id: this.id,
-        lastBootTime: this.lastBootTime,
-        heartbeatInterval: this.heartbeatInterval,
-        bootRetryInterval: this.bootRetryInterval,
-        status: this.status,
-        statusInfo: this.statusInfo,
-        getBaseReportOnPending: this.getBaseReportOnPending,
-        pendingBootSetVariables: this.pendingBootSetVariables,
-        variablesRejectedOnLastBoot: this.variablesRejectedOnLastBoot,
-        bootWithRejectedVariables: this.bootWithRejectedVariables,
-        customData: this.customData,
-      } as Boot;
-    } catch (error) {
-      throw new Error('Convert BootMapper to Boot failed: ' + error);
-    }
+    return Boot.build({
+      id: this.id,
+      status: this.status,
+      variablesRejectedOnLastBoot: this.variablesRejectedOnLastBoot,
+      lastBootTime: this.lastBootTime,
+      heartbeatInterval: this.heartbeatInterval,
+      bootRetryInterval: this.bootRetryInterval,
+      statusInfo: this.statusInfo,
+      getBaseReportOnPending: this.getBaseReportOnPending,
+      pendingBootSetVariables: this.pendingBootSetVariables,
+      bootWithRejectedVariables: this.bootWithRejectedVariables,
+      customData: this.customData,
+    });
   }
 
-  private fromModel(boot: Boot): void {
-    this.lastBootTime = boot.lastBootTime ? boot.lastBootTime : undefined;
-    this.heartbeatInterval = boot.heartbeatInterval;
-    this.bootRetryInterval = boot.bootRetryInterval;
-    this.statusInfo = boot.statusInfo != null ? (boot.statusInfo as OCPP2_0_1.StatusInfoType) : undefined;
-    this.getBaseReportOnPending = boot.getBaseReportOnPending;
-    this.pendingBootSetVariables = boot.pendingBootSetVariables ? (boot.pendingBootSetVariables as OCPP2_0_1.VariableAttributeType[]) : undefined;
-    this.bootWithRejectedVariables = boot.bootWithRejectedVariables;
-    this.customData = boot.customData != null ? (boot.customData as OCPP2_0_1.CustomDataType) : undefined;
+  static fromModel(boot: Boot): BootMapper {
+    return new BootMapper(
+      boot.id,
+      boot.status as OCPP2_0_1.RegistrationStatusEnumType,
+      boot.variablesRejectedOnLastBoot as OCPP2_0_1.SetVariableResultType[],
+      boot.lastBootTime,
+      boot.heartbeatInterval,
+      boot.bootRetryInterval,
+      boot.statusInfo ? (boot.statusInfo as OCPP2_0_1.StatusInfoType) : boot.statusInfo,
+      boot.getBaseReportOnPending,
+      boot.pendingBootSetVariables ? (boot.pendingBootSetVariables as OCPP2_0_1.VariableAttributeType[]) : boot.pendingBootSetVariables,
+      boot.bootWithRejectedVariables,
+      boot.customData ? (boot.customData as OCPP2_0_1.CustomDataType) : boot.customData,
+    );
   }
 }
