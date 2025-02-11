18697b86722e20aae3da1124f6a85daecb3e85fc
Zihe Cheng <lydiazcheng@users.noreply.github.com>
2025-02-11 15:34:42 -0500

    Issue 86: Implement RemoteStopTransaction
    Signed-off-by: Zihe Cheng <lydiazcheng@users.noreply.github.com>

--- 03_Modules/EVDriver/src/index.ts
+++ 03_Modules/EVDriver/src/index.ts
@@ -4,6 +4,7 @@
 // SPDX-License-Identifier: Apache 2.0
 
 export { EVDriverOcpp201Api } from './module/Ocpp201Api';
+export { EVDriverOcpp16Api } from './module/Ocpp16Api';
 export { EVDriverDataApi } from './module/DataApi';
 export { IEVDriverModuleApi } from './module/interface';
 export { EVDriverModule } from './module/module';
