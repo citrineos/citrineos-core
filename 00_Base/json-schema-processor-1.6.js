27f0158698335c7f3b2458e988b1710be1380897
thana.paris@s44.team <tparis@MACUS028.local>
2025-09-12 14:50:34 -0400

    trying to fix license headers

--- 00_Base/json-schema-processor-1.6.js
+++ 00_Base/json-schema-processor-1.6.js
@@ -26,7 +26,7 @@ const globalEnumDefinitions = {};
 
 const licenseComment = `// Copyright Contributors to the CitrineOS Project
 //
-// SPDX-License-Identifier: Apache 2.0
+// SPDX-License-Identifier: Apache-2.0
 
 `;
 
