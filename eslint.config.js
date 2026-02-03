eafd39c4f6e2256a38407b5a69757e4990c6b879
Elliot Sabitov <elliot.sabitov@gmail.com>
2026-02-03 11:18:24 -0500

    chore: removing no longer needed DirectusExtensions

--- eslint.config.js
+++ eslint.config.js
@@ -45,7 +45,6 @@ export default tseslint.config(
   // Ignore patterns
   {
     ignores: [
-      '**/DirectusExtensions/**',
       '**/Swarm/**',
       '**/dist/**',
       '**/node_modules/**',
