ad1c7f5474fe536a85018d00bdbb7caec7777f4c
Thana Paris <thana.paris@s44.team>
2026-02-04 16:38:57 -0500

    converting all meter values properly to native types for ocpp 1.6 and 2.0.1. updating total kwh calculation to only use new meter values with new meterStart field on transaction model.

--- 03_Modules/Transactions/src/module/CostNotifier.ts
+++ 03_Modules/Transactions/src/module/CostNotifier.ts
@@ -1,9 +1,9 @@
 // SPDX-FileCopyrightText: 2025 Contributors to the CitrineOS Project
 //
 // SPDX-License-Identifier: Apache-2.0
+import { AbstractModule, OCPP2_0_1_CallAction, OCPPVersion } from '@citrineos/base';
 import type { ITransactionEventRepository } from '@citrineos/data';
 import { Transaction } from '@citrineos/data';
-import { AbstractModule, OCPP2_0_1_CallAction, OCPPVersion } from '@citrineos/base';
 import type { ILogObj } from 'tslog';
 import { Logger } from 'tslog';
 import { CostCalculator } from './CostCalculator.js';
@@ -56,7 +56,7 @@ export class CostNotifier extends Scheduler {
     const cost = await this._costCalculator.calculateTotalCost(
       tenantId,
       transaction.stationId,
-      transaction.id,
+      transaction.totalKwh!,
     );
 
     await this._transactionEventRepository.updateTransactionTotalCostById(
