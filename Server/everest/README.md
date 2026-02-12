660865025c32d8d6cb1ae1e724120abbb53a5464
Devon Newsom <devon.newsom2@gmail.com>
2026-02-12 14:02:34 -0700

    Updates Everest image to their current latest Exposes ports on 8083 for OCPP2.1 unauthorized messaging Updates checks to allow OCPP2.1 to connect. Further functionality to be added later. Adjusts start.sh script, docker environment, and package.json to more dynamically work with OCPP versions. Completely adjusted how we approach changing the port for Everest's config

--- Server/everest/README.md
+++ Server/everest/README.md
@@ -25,7 +25,7 @@ the `/Server/everest` directory so that it can pick up the `Dockerfile` and the 
 You will notice that there are two args that are configurable:
 
 - `EVEREST_IMAGE_TAG` - The image tag that will be used for the EVerest image (ghcr.io/everest/everest-demo/manager).
-- `EVEREST_TARGET_URL` - The CSMS URL that EVerest will connect to. Defaults to `host.docker.internal` assuming CitrineOS will run on same machine, since `localhost` won't work within Docker.
+- `OCPP_VERSION` - The version of OCPP to run everest for. `start-everest` will handle all 2.x versions for the given 2.x OCPP_VERSION and start-everest-16 is for OCPP1.6 only.
 
 After running `npm run start-everest` (or the Windows alternative), you should see 3 running EVerest containers
 and the `manager` container should have the appropriate EVerest logs.
--- Server/everest/start.sh
+++ Server/everest/start.sh
@@ -3,26 +3,71 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-if [ "$OCPP_VERSION" = "two" ]; then
-    apt-get update && apt-get install -y sqlite3
-    sqlite3 /ext/dist/share/everest/modules/OCPP201/device_model_storage.db \
-            "UPDATE VARIABLE_ATTRIBUTE \
-            SET value = '[{\"configurationSlot\": 1, \"connectionData\": {\"messageTimeout\": 30, \"ocppCsmsUrl\": \"$EVEREST_TARGET_URL\", \"ocppInterface\": \"Wired0\", \"ocppTransport\": \"JSON\", \"ocppVersion\": \"OCPP20\", \"securityProfile\": 1}},{\"configurationSlot\": 2, \"connectionData\": {\"messageTimeout\": 30, \"ocppCsmsUrl\": \"$EVEREST_TARGET_URL\", \"ocppInterface\": \"Wired0\", \"ocppTransport\": \"JSON\", \"ocppVersion\": \"OCPP20\", \"securityProfile\": 1}}]' \
-            WHERE \
-            variable_Id IN ( \
-            SELECT id FROM VARIABLE \
-            WHERE name = 'NetworkConnectionProfiles' \
-            );"
+_OCPP_VERSION=$OCPP_VERSION
+OCPP_VERSION_ENUM="OCPP201"
+EVEREST_TARGET_URL="ws://host.docker.internal:8081/cp001"
+
+case "$_OCPP_VERSION" in
+  "1.6")
+    OCPP_VERSION_ENUM="OCPP16"
+    EVEREST_TARGET_URL="ws://host.docker.internal:8092/"
+    ;;
+"2.0.1")
+    OCPP_VERSION_ENUM="OCPP201"
+    ;;
+"2.1")
+    OCPP_VERSION_ENUM="OCPP21"
+    EVEREST_TARGET_URL="ws://host.docker.internal:8083/cp001"
+    ;;
+  *)
+    # NOT in the list
+    _OCPP_VERSION="2.0.1"
+    OCPP_VERSION_ENUM="OCPP201"
+    ;;
+esac
+
+if [ "$_OCPP_VERSION" != "1.6" ]; then
+    #There are two different configs in Everest that default their settings that we need to override for Citrine.
+    #The first is a temp file that may or may not be used by Everest, but to be safe we change the config there.
+    #The second is the config used by Everest when setting up their image, and this is the primary driver for the configuration of the image.
+
+    CONFIG="$(cat <<JSON
+[{"configurationSlot": 1, "connectionData": {"messageTimeout": 30, "ocppCsmsUrl": "$EVEREST_TARGET_URL", "ocppInterface": "Wired0", "ocppTransport": "JSON", "ocppVersion": "$OCPP_VERSION_ENUM", "securityProfile": 1}}, {"configurationSlot": 2, "connectionData": {"messageTimeout": 30, "ocppCsmsUrl": "$EVEREST_TARGET_URL", "ocppInterface": "Wired0", "ocppTransport": "JSON", "ocppVersion": "$OCPP_VERSION_ENUM", "securityProfile": 2}}]
+JSON
+ )"
+
+    chmod +x /tmp/config.json
+    jq --argjson config "$CONFIG" '
+    (.[] 
+        | select(.name == "InternalCtrlr") 
+        | .variables.NetworkConnectionProfiles.attributes.Actual
+    ) = $config
+    ' "/tmp/config.json" > /tmp/config_citrine.json && mv /tmp/config_citrine.json "/tmp/config.json"
+    chmod -x /tmp/config.json
+
+    chmod +x /ext/dist/share/everest/modules/OCPP201/component_config/standardized/InternalCtrlr.json
+    jq --argjson config "$CONFIG" '
+    (. 
+        | .properties 
+        | .NetworkConnectionProfiles 
+        | .attributes[] 
+        | select(.type == "Actual") 
+        | .value
+    ) = $config
+    ' "/ext/dist/share/everest/modules/OCPP201/component_config/standardized/InternalCtrlr.json" \
+    > /tmp/config_citrine_dist.json && mv /tmp/config_citrine_dist.json "/ext/dist/share/everest/modules/OCPP201/component_config/standardized/InternalCtrlr.json"
+    chmod -x /ext/dist/share/everest/modules/OCPP201/component_config/standardized/InternalCtrlr.json
 fi
 
 /entrypoint.sh
 http-server /tmp/everest_ocpp_logs -p 8888 &
 
-if [ "$OCPP_VERSION" = "one" ]; then
+if [ "$_OCPP_VERSION" = "1.6" ]; then
     chmod +x /ext/build/run-scripts/run-sil-ocpp.sh
     sed -i "0,/127.0.0.1:8180\/steve\/websocket\/CentralSystemService\// s|127.0.0.1:8180/steve/websocket/CentralSystemService/|${EVEREST_TARGET_URL}|" /ext/dist/share/everest/modules/OCPP/config-docker.json
     /ext/build/run-scripts/run-sil-ocpp.sh
 else
+    #Works for all 2.x versions
     rm /ext/dist/share/everest/modules/OCPP201/component_config/custom/EVSE_2.json
     rm /ext/dist/share/everest/modules/OCPP201/component_config/custom/Connector_2_1.json
     chmod +x /ext/build/run-scripts/run-sil-ocpp201-pnc.sh
