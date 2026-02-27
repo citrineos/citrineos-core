40b2d411bc3716e15c48ea0ca12a3d8eb856e2dd
Elliot Sabitov <elliot.sabitov@gmail.com>
2026-02-27 13:07:29 -0500

    chore: moving pm2 to server for cleanliness and made start-docker-cloud run pm2

--- Server/ecosystem.config.cjs
+++ Server/ecosystem.config.cjs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Contributors to the CitrineOS Project
+//
+// SPDX-License-Identifier: Apache-2.0
+
 // PM2 ecosystem file for CitrineOS core Server
 // Runs the built Server app (./Server/dist/index.js) in cluster mode across CPUs.
 module.exports = {
@@ -17,9 +21,12 @@ module.exports = {
       error_file: './logs/citrineos-error.log',
       out_file: './logs/citrineos-out.log',
       // Environment variables for production run; PM2 will pick the 'production' env by default when --env production is used
+      node_args: '--inspect=0.0.0.0:9229',
       env_production: {
-        APP_ENV: 'production',
+        APP_ENV: 'local',
         APP_NAME: 'all',
+        BOOTSTRAP_CITRINEOS_FILE_ACCESS_TYPE: 'local',
+        BOOTSTRAP_CITRINEOS_FILE_ACCESS_LOCAL_DEFAULT_FILE_PATH: '/data',
       },
     },
   ],
