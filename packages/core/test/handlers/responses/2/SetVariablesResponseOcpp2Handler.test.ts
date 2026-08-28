b706c239e96da159a01ab0c226cbc7956e8165e9
Venus Yu <venus0yu@gmail.com>
2026-08-28 15:34:12 -0400

    feature: fix missing tenant name check in tests

--- packages/core/test/handlers/responses/2/SetVariablesResponseOcpp2Handler.test.ts
+++ packages/core/test/handlers/responses/2/SetVariablesResponseOcpp2Handler.test.ts
@@ -127,7 +127,7 @@ function makeHandler(): SetVariablesResponseOcpp2Handler {
 // ---------------------------------------------------------------------------
 
 async function seedBase(): Promise<void> {
-  await Tenant.create({ id: TENANT_ID as any });
+  await Tenant.create({ id: TENANT_ID as any, name: TENANT_ID });
   await ChargingStation.create({
     ocppConnectionName: OCPP_CONNECTION_NAME,
     isOnline: false,
