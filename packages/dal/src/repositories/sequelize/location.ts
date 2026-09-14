e58cd1b4734faf9fa2946facf029395417cfd7f6
vicky-ahmed <vicky.ahmed@s44.team>
2026-09-14 16:48:09 -0400

    fix(dal): de-duplicate readConnectorsByStationId from a bad merge

--- packages/dal/src/repositories/sequelize/location.ts
+++ packages/dal/src/repositories/sequelize/location.ts
@@ -489,15 +489,6 @@ export class SequelizeLocationRepository
     );
   }
 
-  async readConnectorsByStationId(
-    tenantId: number,
-    ocppConnectionName: string,
-  ): Promise<ConnectorDto[]> {
-    return await Connector.findAll({
-      where: { tenantId, ocppConnectionName },
-    });
-  }
-
   async readEvseByStationIdAndOcpp201EvseId(
     tenantId: number,
     ocppConnectionName: string,
